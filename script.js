df89344b0a75cbe3ad629e753b43b528fc0d1f0a
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2023-12-19 00:21:30 -0500

    Formatted js file with Prettier for consistency

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
 // Create an array to hold values
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ["rock", "paper", "scissors"];
 const maxScore = 5;
 
 // Start game
 game();
 
-
 // Simulate a game
 function game() {
-    // Initialize variables
-    let userScore = 0;
-    let computerScore = 0;
+  // Initialize variables
+  let userScore = 0;
+  let computerScore = 0;
 
-    // Loop over rounds while user and computer score are below the max score
-    for (let i = 0; userScore < maxScore && computerScore < maxScore; i++) {
-        // Obtain user input and account for case sensitivity
-        let playerSelection = prompt(`Select 'Rock' 'Paper' or Scissors`).toLowerCase();
+  // Loop over rounds while user and computer score are below the max score
+  for (let i = 0; userScore < maxScore && computerScore < maxScore; i++) {
+    // Obtain user input and account for case sensitivity
+    let playerSelection = prompt(
+      `Select 'Rock' 'Paper' or Scissors`
+    ).toLowerCase();
 
-        // Obtain the round outcome
-        let computerSelection = getComputerChoice();
-        roundOutcome = playRound(playerSelection, computerSelection);
-        
-        // Tally and increment the round winner
-        if (roundOutcome !== 'Draw') {
-            roundOutcome === 'You Win' ? userScore++ : computerScore++;
-        }
+    // Obtain the round outcome
+    let computerSelection = getComputerChoice();
+    roundOutcome = playRound(playerSelection, computerSelection);
 
-        // Log the round outcome
-        console.log(roundOutcome);
+    // Tally and increment the round winner
+    if (roundOutcome !== "Draw") {
+      roundOutcome === "You Win" ? userScore++ : computerScore++;
     }
-    // Declare the winner depending on who reached it first
-    userScore === maxScore ? console.log(`Player wins ${userScore} to ${computerScore}!`) : console.log(`Computer wins ${computerScore} to ${userScore}!`)
-}
 
+    // Log the round outcome
+    console.log(roundOutcome);
+  }
+  // Declare the winner depending on who reached it first
+  userScore === maxScore
+    ? console.log(`Player wins ${userScore} to ${computerScore}!`)
+    : console.log(`Computer wins ${computerScore} to ${userScore}!`);
+}
 
 // Obtain computer's choice
 function getComputerChoice() {
-    // Randomly roll a number between 0-2
-    let roll = Math.floor(Math.random() * 3);
-    
-    // Select choice from array index based on roll
-    let choice = choices[roll];
-    return choice;
-}
+  // Randomly roll a number between 0-2
+  let roll = Math.floor(Math.random() * 3);
 
+  // Select choice from array index based on roll
+  return computerChoice = choices[roll];
+}
 
 // Simulate a round
 function playRound(playerSelection, computerSelection) {
-    // If its the same selection, return a draw
-    if (playerSelection === computerSelection) {
-        return 'Draw';
-    }
+  // If its the same selection, return a draw
+  if (playerSelection === computerSelection) {
+    return "Draw";
+  }
 
-    // Account for possible scenarios
-    if (playerSelection === 'rock') {
-        const result = computerSelection === 'paper' ? 'You Lose' : 'You Win';
-        return result
-    } else if (playerSelection === 'paper') {
-        const result = computerSelection === 'scissors' ? 'You Lose' : 'You Win';
-        return result;
-    } else {
-        const result = computerSelection === 'rock' ? 'You Lose' : 'You Win';
-        return result
-    }
+  // Account for possible scenarios
+  if (playerSelection === "rock") {
+    const result = computerSelection === "paper" ? "You Lose" : "You Win";
+    return result;
+  } else if (playerSelection === "paper") {
+    const result = computerSelection === "scissors" ? "You Lose" : "You Win";
+    return result;
+  } else {
+    const result = computerSelection === "rock" ? "You Lose" : "You Win";
+    return result;
+  }
 }
