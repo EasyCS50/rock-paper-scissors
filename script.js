d120e06d3ba5dd7ea4b7909dedbf71c7c226ff40
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-01-03 18:32:02 -0500

    Changed all console.log's into DOM methods

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Create const global array and max score
 const CHOICES = ["rock", "paper", "scissors"];
 const MAX_SCORE = 5;
-const buttons = document.querySelectorAll("button");
 
 // Initialize variables
 let userScore = 0;
 let computerScore = 0;
+
+const buttons = document.querySelectorAll("button");
+const container = document.querySelector("#container");
+
+const scores = document.createElement("p");
+container.appendChild(scores);
+
+const results = document.createElement("p");
+container.appendChild(results);
 
 // Start game
 game();
@@ -14,20 +22,24 @@ game();
 function game() {
   buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
-      let playerSelection = event.target.id;
+      let userSelection = event.target.id;
       let computerSelection = getComputerChoice();
 
-      let roundOutcome = playRound(playerSelection, computerSelection);
+      let roundOutcome = playRound(userSelection, computerSelection);
 
       // Tally and increment the round winner
-      if (roundOutcome !== "Draw") {
-        roundOutcome === "You Win" ? userScore++ : computerScore++;
+      if (!roundOutcome.includes(`Draw`)) {
+        roundOutcome.includes(`Win`) ? userScore++ : computerScore++;
       }
-      console.log(roundOutcome);
+
+      scores.textContent = `User score: ${userScore}, Computer score: ${computerScore}`;
+      results.textContent = roundOutcome;
 
       // Prevent game from proceeding
       if (userScore === MAX_SCORE || computerScore === MAX_SCORE) {
-        declairWinner();
+        userScore === MAX_SCORE
+          ? (scores.textContent = `Player wins ${userScore} to ${computerScore}!`)
+          : (scores.textContent = `Computer wins ${computerScore} to ${userScore}!`);
 
         buttons.forEach((button) => {
           button.disabled = true;
@@ -42,25 +54,27 @@ function getComputerChoice() {
   return (computerChoice = CHOICES[roll]);
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(userSelection, computerSelection) {
   // If its the same selection, return a draw
-  if (playerSelection === computerSelection) {
-    return "Draw";
+  if (userSelection === computerSelection) {
+    return `Draw! You both have chosen ${userSelection}.`;
   }
 
   // Account for possible scenarios
-  if (playerSelection === "rock") {
-    return (result = computerSelection === "paper" ? "You Lose" : "You Win");
-  } else if (playerSelection === "paper") {
-    return (result = computerSelection === "scissors" ? "You Lose" : "You Win");
+  if (userSelection === "rock") {
+    return (result =
+      computerSelection === "paper"
+        ? `You Lose! ${computerSelection} beats ${userSelection}.`
+        : `You Win! ${userSelection} beats ${computerSelection}.`);
+  } else if (userSelection === "paper") {
+    return (result =
+      computerSelection === "scissors"
+        ? `You Lose! ${computerSelection} beats ${userSelection}.`
+        : `You Win! ${userSelection} beats ${computerSelection}.`);
   } else {
-    return (result = computerSelection === "rock" ? "You Lose" : "You Win");
+    return (result =
+      computerSelection === "rock"
+        ? `You Lose! ${computerSelection} beats ${userSelection}.`
+        : `You Win! ${userSelection} beats ${computerSelection}.`);
   }
-}
-
-function declairWinner() {
-  // Declare the winner depending on who reached it first
-  userScore === MAX_SCORE
-    ? console.log(`Player wins ${userScore} to ${computerScore}!`)
-    : console.log(`Computer wins ${computerScore} to ${userScore}!`);
 }
